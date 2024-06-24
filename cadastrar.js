a5c9035bc1b30efc16e010ca3da121c829dfa7aa
Marcos <marcos.gabrielr150@gmail.com>
2024-06-24 16:51:14 -0300

    commit

---
+++ cadastrar.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cadastroForm = document.getElementById('cadastroForm');
+
+    cadastroForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const nome = document.getElementById('nome').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const telefone = document.getElementById('telefone').value.trim();
+
+        // Validação simples dos campos
+        if (!nome || !email || !telefone) {
+            alert('Todos os campos são obrigatórios');
+            return;
+        }
+
+        const userData = {
+            name: nome,
+            email: email,
+            call: telefone
+        };
+
+        console.log('Dados do usuário a serem enviados:', userData);
+
+        fetch('http://localhost:3333/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxNTA4NWJlMy0wNzViLTRjNjAtYTM3Ni1jMGRkY2JjYWI3ZmMiLCJyb2xlcyI6dHJ1ZSwiaWF0IjoxNzE4OTkyNTc0fQ.SvKqyRf3YZG8zTaItqSuXK0ljw5nNe6jXMf2RalyDfY' 
+            },
+            body: JSON.stringify(userData)
+        })
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(error => {
+                    console.log('Erro detalhado:', error);
+                    throw new Error(error.message || 'Erro ao cadastrar o usuário');
+                });
+            }
+            return response.json();
+        })
+        .then(newUser => {
+            console.log('Usuário cadastrado com sucesso:', newUser);
+            window.location.href = './index.html'; // Redirecionar para a página inicial após o cadastro
+        })
+        .catch(error => {
+            console.error('Erro ao cadastrar o usuário:', error);
+            alert('Erro ao cadastrar o usuário: ' + error.message);
+        });
+    });
+
+    const btncancelar = document.getElementById('btncancelar');
+    btncancelar.addEventListener('click', function() {
+        window.location.href = './index.html';
+    });
+});
